3f041fc103c00d7cb2b86cbefcb3ac4d82ccb206
Aniket Khaladkar <khaladkaraniket0@gmail.com>
2025-11-14 10:43:50 +0000

    Take Asian Paints Logo from internet and add beside i-Safe

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'github.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
         port: '',
         pathname: '/**',
       }
