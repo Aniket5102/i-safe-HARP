70eb0d37876a6340d7654750911f714579a039b0
Aniket Khaladkar <khaladkaraniket0@gmail.com>
2025-11-21 11:23:29 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,12 @@
-
-// This file is intentionally left blank. 
-// It will be populated with your Firebase project's configuration 
-// when you connect to a Firebase project.
 import { FirebaseOptions } from 'firebase/app';
 
-const firebaseConfig: FirebaseOptions = {};
+const firebaseConfig: FirebaseOptions = {
+  projectId: "studio-8489511678-2ef5f",
+  appId: "1:821588430150:web:14331aa9c4547c7d16e947",
+  apiKey: "AIzaSyBfp9UuzUQ0RkdNqVVjWrj_96mkhVhewec",
+  authDomain: "studio-8489511678-2ef5f.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "821588430150"
+};
 
 export default firebaseConfig;
