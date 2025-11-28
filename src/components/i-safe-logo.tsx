1a48f77e45ceaed00bac18a53fa3b0717e45b75b
Aniket Khaladkar <khaladkaraniket0@gmail.com>
2025-11-28 08:04:55 +0000

    Increase size of i-safe logo https://www.bing.com/images/search?view=det

--- src/components/i-safe-logo.tsx
+++ src/components/i-safe-logo.tsx
@@ -5,8 +5,8 @@ const ISafeLogo = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
   <img
     src="https://img.freepik.com/premium-vector/safety-first-logo-design-shield-helmet-construction-safely-workplace-factory-logo-banner_521317-1381.jpg"
     alt="i-safe Logo"
-    width={20}
-    height={20}
+    width={40}
+    height={40}
     {...props}
   />
 );
