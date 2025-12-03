49d9b5ebe36d7fd5926d2be0070b2026b15578dc
Shriyash Karekar <yashkarekar231@gmail.com>
2025-12-03 10:30:35 +0000

    page not fully occupied

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -308,13 +308,11 @@ const SidebarInset = React.forwardRef<
   HTMLDivElement,
   React.ComponentProps<"main">
 >(({ className, ...props }, ref) => {
-  const { open } = useSidebar();
-  
   return (
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-background transition-[margin-left] duration-300 ease-in-out",
+        "relative flex min-h-svh flex-1 flex-col bg-background",
         "md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
