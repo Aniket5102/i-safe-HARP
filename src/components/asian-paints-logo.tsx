96457882091fd0c6f449ff23d14b1ea802ae09b3
Aniket Khaladkar <khaladkaraniket0@gmail.com>
2025-12-09 04:39:03 +0000

    Reduce size Asian Paints logo height

--- src/components/asian-paints-logo.tsx
+++ src/components/asian-paints-logo.tsx
@@ -5,8 +5,8 @@ const AsianPaintsLogo = (props: Omit<React.ComponentProps<typeof Image>, 'src' |
   <Image
     src="/asian-paints-logo.png"
     alt="Asian Paints Logo"
-    width={120}
-    height={18}
+    width={80}
+    height={12}
     {...props}
   />
 );
