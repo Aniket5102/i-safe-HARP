c59c3105b33b5274bc1f54156657dc12b679911c
Aniket Khaladkar <khaladkaraniket0@gmail.com>
2025-12-10 02:43:06 +0000

    Need toggle in right side top in settings to change website theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from '@/context/auth-context';
 import ConditionalSidebarLayout from '@/components/conditional-sidebar-layout';
+import { ThemeProvider } from '@/context/theme-provider';
 
 
 export const metadata: Metadata = {
@@ -17,19 +18,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" style={{ colorScheme: 'dark' }} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased bg-background">
-        <AuthProvider>
-          <ConditionalSidebarLayout>
-            {children}
-          </ConditionalSidebarLayout>
-          <Toaster />
-        </AuthProvider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <AuthProvider>
+            <ConditionalSidebarLayout>
+                {children}
+            </ConditionalSidebarLayout>
+            <Toaster />
+            </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
