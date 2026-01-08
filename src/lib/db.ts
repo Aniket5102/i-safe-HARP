f9d9e0990956629b296284907b9267166206b966
Aniket Khaladkar <khaladkaraniket0@gmail.com>
2026-01-08 08:54:51 +0000

    Error: POSTGRES_URL environment variable is not set. Please create a .en

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,7 @@
 import { Pool } from 'pg';
+import { config } from 'dotenv';
+
+config({ path: '.env.local' });
 
 // This is a singleton to ensure we only have one pool instance.
 let pool: Pool | undefined;
